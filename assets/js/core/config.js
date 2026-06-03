e6ea19ee1d1a78f3e2a38cfa4443ef0aaa0c2624
itachidotasm <sharmaps1758@gmail.com>
2026-06-03 16:03:50 +0530

    Add Narayan7330 to contributors list
    kanged Prev. year papers from Narayan7330's jecrc-pyq repo

--- assets/js/core/config.js
+++ assets/js/core/config.js
@@ -4,7 +4,7 @@ window.JUAssignmentsApp = window.JUAssignmentsApp || {};
   app.config = {
     manifestPath: "assignments/manifest.json",
     repoName: "Whit31ister/JU_ASSIGN",
-    contributors: ["Whit31ister","itachidotasm","voidshekhawat"], // Add GitHub usernames here to list them as contributors
+    contributors: ["Whit31ister","itachidotasm","voidshekhawat","Narayan7330"], // Add GitHub usernames here to list them as contributors
     acronymTokens: ["ai", "cn", "coa", "css", "daa", "dbms", "dsa", "ml", "oops", "os", "ui", "ux"]
   };
 })(window.JUAssignmentsApp);
